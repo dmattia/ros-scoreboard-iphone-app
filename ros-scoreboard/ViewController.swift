c95cbe3e0fd5dc30e2188ae7993cfd14e14c4863
dmattia <dmattia@nd.edu>
2016-03-16 14:59:05 -0400

    Added button that connects a label to a firebase backend

--- ros-scoreboard/ViewController.swift
+++ ros-scoreboard/ViewController.swift
@@ -7,19 +7,32 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textLabel: UILabel!
+    private var players : Dictionary<String, Int>?
+    let ref = Firebase(url:"https://ros.firebaseio.com")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        ref.observeEventType(.Value, withBlock: {
+            snapshot in
+            self.players = snapshot.value as? Dictionary<String, Int>
+            self.reloadViews()
+        })
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func reloadViews() {
+        let blueScore = self.players!["Blue Player"]
+        self.textLabel.text = "\(blueScore!)"
     }
-
-
+    
+    @IBAction func upButtonPressed(sender: AnyObject) {
+        self.players!["Blue Player"] = self.players!["Blue Player"]! + 1
+        ref.setValue(players)
+    }
 }
 
---
+++ ros-scoreboard/ros-scoreboard-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  ros-scoreboard-Bridging-Header.h
+//  ros-scoreboard
+//
+//  Created by David Mattia on 3/16/16.
+//  Copyright © 2016 David Mattia. All rights reserved.
+//
+
+#ifndef ros_scoreboard_Bridging_Header_h
+#define ros_scoreboard_Bridging_Header_h
+
+#import <Firebase/Firebase.h>
+
+#endif /* ros_scoreboard_Bridging_Header_h */
